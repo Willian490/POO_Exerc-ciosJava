4aa080104e84b6b162f5e92a56b0bf4aae2b6194
Willian490 <williandeoliveiramarquesolivei@gmail.com>
2024-09-14 16:42:18 -0300

    correçaõ de exercícios

--- Exercicio1$4.java
+++ Exercicio1$4.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
 
 public class Exercicio1$4 {
     public static void main(String[] args) {
         
         System.out.println("       *");
+        System.out.println("      *");
+        System.out.println("     *");
+        System.out.println("*   *");
+        System.out.println(" * *");
+        System.out.println("  *");
+
+
     }
 }
